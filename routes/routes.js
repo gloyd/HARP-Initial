2514d49de4844f1c6a06ba8bc2832b820ca4023f
jbrysch <jbrysch@gmail.com>
2015-12-29 15:36:54 -0600

    Restarted

--- routes/routes.js
+++
@@ -1,9 +0,0 @@
-var appRouter = function(app) {
-	// var actionRoutes = require("./actionRoutes.js")(app);
-	var adminRoutes = require("./adminRoutes.js")(app);
-	// var maintenanceRoutes = require("./maintenanceRoutes.js")(app);
-	var debugRoutes = require("./debugRoutes.js")(app);
-
-}
-
-module.exports = appRouter;
