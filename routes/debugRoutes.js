c1b5367e95681b3bf9d2034784a554f6c90853a5
jbrysch <jbrysch@gmail.com>
2015-12-29 15:25:01 -0600

    adding server instead of app

--- routes/debugRoutes.js
+++ routes/debugRoutes.js
@@ -1,5 +1,10 @@
 var appDebugRouter = function(app) {
- 
+
+ // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+app.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
 app.get("/hello", function(req, res) {
     res.send("Hello World");
 });
--- server.js
+++ server.js
@@ -18,13 +18,8 @@ var port = process.env.PORT || 8080;        // set our port
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
+var routes = require("./routes/routes.js")(app);
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
-
-// more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
