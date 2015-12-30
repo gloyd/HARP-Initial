e588e49c6d95abde0dad0fd7020c55edd7c0cf4d
jbrysch <jbrysch@gmail.com>
2015-12-29 20:39:06 -0600

    UPDATE
    removed semi colon

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ router.route('/nodes')
         //         res.send(err);
 
         //     res.json({ message: 'Bear created!' });
-        });
+        })
 
         // get all the bears (accessed at GET http://localhost:8080/api/bears)
     .get(function(req, res) {
@@ -74,7 +74,7 @@ router.route('/node')
         //         res.send(err);
 
         //     res.json({ message: 'Bear created!' });
-        });
+        })
 
         // get all the bears (accessed at GET http://localhost:8080/api/bears)
     .get(function(req, res) {
