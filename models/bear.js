15ade8cc1415da9fa42c7ea146d54893870e0a86
jbrysch <jbrysch@gmail.com>
2015-12-29 20:50:40 -0600

    adding bear
    test

--- models/bear.js
+++ models/bear.js
@@ -1,1 +1,8 @@
-raspNode.js
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var BearSchema   = new Schema({
+    name: String
+});
+
+module.exports = mongoose.model('Bear', BearSchema);
