165b7313eead5a193e549bf28bf5617633866336
jbrysch <jbrysch@gmail.com>
2016-01-04 20:28:53 -0600

    Logging response
    Test

--- HARP-Client/HARP-C/client.js
+++ HARP-Client/HARP-C/client.js
@@ -22,6 +22,7 @@ var router = express.Router();              // get an instance of the express Ro
 router.use(function(req, res, next) {
     // do logging
     console.log('Something is happening.');
+    console.log('Response - '+ res);
     next(); // make sure we go to the next routes and don't stop here
 });
 
