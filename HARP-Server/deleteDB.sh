76b0344889cbb213ec47ab595517cc09277176ac
jbrysch <jbrysch@gmail.com>
2016-01-04 21:50:48 -0600

    Junk
    testing

---
+++ HARP-Server/deleteDB.sh
@@ -0,0 +1,8 @@
+###################################
+# Filename:	deleteDB.sh
+# Author:	J. Brysch
+# Date :	01/04/2016
+###################################
+#./HARP-Client/deleteDB.sh
+./HARP-Server/deleteDB.sh
+
---
+++ call-it-a-day.sh
@@ -0,0 +1,8 @@
+###################################
+# Filename:	call-it-a-day.sh
+# Author:	J. Brysch
+# Date :	01/04/2016
+###################################
+#./HARP-Client/deleteDB.sh
+./HARP-Server/deleteDB.sh
+
